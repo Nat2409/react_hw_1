341aa3501b8992c639da0e17f491953bda728d40
Nat2409 <soni.box85@gmail.com>
2022-08-20 17:07:02 +0300

    Updates

---
+++ precache-manifest.e3b0243688606e35b70f57d932dd55f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f94b22cc44e2ff2df31",
+    "url": "/react_hw_1/static/css/main.b9090720.chunk.css"
+  },
+  {
+    "revision": "8f94b22cc44e2ff2df31",
+    "url": "/react_hw_1/static/js/main.8f94b22c.chunk.js"
+  },
+  {
+    "revision": "c023cc881a9deab9c0e9",
+    "url": "/react_hw_1/static/js/1.c023cc88.chunk.js"
+  },
+  {
+    "revision": "5edb2abfeb1bedfe4db2",
+    "url": "/react_hw_1/static/js/runtime~main.5edb2abf.js"
+  },
+  {
+    "revision": "80f9155573f95f448fb1cb7d913db5e3",
+    "url": "/react_hw_1/index.html"
+  }
+];
